b660cf1384bb954daddb31dd5ba8dba03c00ec57
Manoj Kumar M <113245138+manojkumarm6m@users.noreply.github.com>
2024-08-21 10:44:39 +0530

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,13 +6,6 @@ from pages.login_page import LoginPage
 import json
 
 
-@pytest.mark.usefixtures("setup_teardown")
-class TestLogin:
-    def test_login_successful(self):
-        pass
-        # time.sleep(15)
-        # title = self.driver.title
-        # expected_title = "Get started"
-        # assert expected_title == title, f"Expected title '{expected_title}' but got '{title}'"
+
 
 
